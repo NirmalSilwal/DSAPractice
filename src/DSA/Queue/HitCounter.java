c148d2d1309020546ac9c316cab7e22510c1ed73
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-03-26 23:53:47 -0500

    Design hit counter data structure given stream of API hit timestamps in seconds

--- src/DSA/Queue/HitCounter.java
+++ src/DSA/Queue/HitCounter.java
@@ -1,5 +1,8 @@
 package DSA.Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 You are given a stream of API hit timestamps in seconds. Design a data structure that supports:
 
@@ -19,4 +22,34 @@ getHits(301) -> 3
 At 301, the hit at 1 is outside the last 300 seconds window.
  */
 public class HitCounter {
+    private final Queue<Integer> queue;
+
+    public HitCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public void hit(int timestamp) {
+        queue.offer(timestamp);
+    }
+
+    public int getHits(int timestamp) {
+        // remove hits older than 5 mins / 300 secs
+        while (!queue.isEmpty() && queue.peek() <= timestamp - 300) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+
+    public static void main(String[] args) {
+        HitCounter counter = new HitCounter();
+        counter.hit(1);
+        counter.hit(2);
+        counter.hit(3);
+
+        System.out.println(counter.getHits(4));
+
+        counter.hit(300);
+        System.out.println(counter.getHits(300));
+        System.out.println(counter.getHits(301));
+    }
 }
