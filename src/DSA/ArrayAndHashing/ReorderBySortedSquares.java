9a9ca50529e280f9017ed024d8e6fe44749c1348
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-03-28 23:14:57 -0500

    Reorder the sorted array based on sorted order of their squares

---
+++ src/DSA/ArrayAndHashing/ReorderBySortedSquares.java
@@ -0,0 +1,9 @@
+package DSA.ArrayAndHashing;
+
+/*
+Given a sorted array, reorder the original elements based on the sorted order of their squares.
+Input:  [-3, -2, -1, 0, 1, 2, 3, 4]
+Output: [0, -1, 1, -2, 2, -3, 3, 4]
+ */
+public class ReorderBySortedSquares {
+}
