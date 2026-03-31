1ae02c872df9dee252626673de77748cc9112d41
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-03-30 23:42:02 -0500

    Minimum window substring problem

---
+++ src/DSA/SlidingWindow/MinimumWindowSubstring.java
@@ -0,0 +1,11 @@
+package DSA.SlidingWindow;
+
+/*
+Given two strings s and t, return the smallest substring of s that contains all characters of t including duplicates.
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+
+https://leetcode.com/problems/minimum-window-substring/description/
+ */
+public class MinimumWindowSubstring {
+}
