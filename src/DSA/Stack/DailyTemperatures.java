e59f46dd2e4cb46e294cf4cf7607b3bc9a3c2167
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-02 23:46:21 -0500

    Daily temperature problem statement

---
+++ src/DSA/Stack/DailyTemperatures.java
@@ -0,0 +1,14 @@
+package DSA.Stack;
+
+/*
+Given an array temperatures, return an array answer such that:
+
+answer[i] = number of days you have to wait after day i to get a warmer temperature
+if there is no future warmer day, answer[i] = 0
+Example
+Input:  [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+ */
+
+public class DailyTemperatures {
+}
