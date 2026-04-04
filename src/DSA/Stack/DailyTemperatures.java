a7b70df37c85430f51eaa21257eb679654f7d70a
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-03 21:40:03 -0500

    Daily temperature solution using monotonic decreasing stack

--- src/DSA/Stack/DailyTemperatures.java
+++ src/DSA/Stack/DailyTemperatures.java
@@ -10,5 +10,32 @@ Input:  [73,74,75,71,69,72,76,73]
 Output: [1,1,4,2,1,1,0,0]
  */
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class DailyTemperatures {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        // store indices
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+    }
 }
