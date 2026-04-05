4e89c29b29330daf4458be11c0bed2c841c863c3
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-04 23:31:20 -0500

    Find next greater element in array - problem statement

---
+++ src/DSA/Stack/NextGreaterElement.java
@@ -0,0 +1,10 @@
+package DSA.Stack;
+
+/*
+Given an integer array nums, return an array result where:
+result[i] = first greater element to the right of nums[i]
+if no greater element exists, return -1
+ */
+
+public class NextGreaterElement {
+}
