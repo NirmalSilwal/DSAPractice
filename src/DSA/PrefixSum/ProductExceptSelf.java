6dbe22ff2294d39a5b10b16dfe719edbe4d81ac7
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-07 20:46:28 -0500

    Find product of all element of array except itself, using prefix and suffix product approach

---
+++ src/DSA/PrefixSum/ProductExceptSelf.java
@@ -0,0 +1,40 @@
+package DSA.PrefixSum;
+
+import java.util.Arrays;
+
+/*
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+
+Input: nums = [1,2,4,6]
+
+Output: [48,24,12,8]
+ */
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n =  nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] output = new int[n];
+
+        prefix[0] = 1;
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        suffix[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            output[i] = prefix[i] * suffix[i];
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2,4, 6};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
