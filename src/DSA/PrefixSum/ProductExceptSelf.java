5060377bdcad366e9aba4d5260a10e54b43288f0
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-07 21:38:54 -0500

    Find product of all element of array except itself, optimized for space

--- src/DSA/PrefixSum/ProductExceptSelf.java
+++ src/DSA/PrefixSum/ProductExceptSelf.java
@@ -33,8 +33,28 @@ public class ProductExceptSelf {
         return output;
     }
 
+    public static int[] productExceptSelfOptimized(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2,4, 6};
         System.out.println(Arrays.toString(productExceptSelf(nums)));
+        System.out.println(Arrays.toString(productExceptSelfOptimized(nums)));
     }
 }
