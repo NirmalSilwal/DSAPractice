e6d7c16159ef7d5e37d28d470300e73535539701
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-18 22:39:36 -0500

    Knapsack problem to pick items within weight limtis

--- src/DSA/KnapsackProblem/PickItemsWithinWeightLimit.java
+++ src/DSA/KnapsackProblem/PickItemsWithinWeightLimit.java
@@ -6,4 +6,25 @@ return the maximum total value you can get without exceeding capacity
  */
 public class PickItemsWithinWeightLimit {
 
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        // dp[i] is max value we can get using capacity i
+        int[] dp = new int[capacity + 1];
+
+        for (int k = 0; k < weights.length; k++) {
+            int w = weights[k];
+            int v = values[k];
+
+            for (int i = capacity; i >= w; i--) {
+                dp[i] = Math.max(dp[i], v + dp[i - w]);
+            }
+        }
+        return dp[capacity];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {1, 3, 4, 5};
+        int[] values = {1, 4, 5, 7};
+        int capacity = 7;
+        System.out.println(knapsack(weights, values, capacity));
+    }
 }
