bd98be7d4ad6130be9674fe95e98cf9297534f0e
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2026-04-18 22:19:59 -0500

    Knapsack problem practice problem

---
+++ src/DSA/KnapsackProblem/PickItemsWithinWeightLimit.java
@@ -0,0 +1,9 @@
+package DSA.KnapsackProblem;
+
+/*
+you are given weights, values, capacity. Each item can be picked only once, and it has weight and value
+return the maximum total value you can get without exceeding capacity
+ */
+public class PickItemsWithinWeightLimit {
+
+}
